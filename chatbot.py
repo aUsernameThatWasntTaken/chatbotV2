8db943c88b03224feb907c35e1dd1b2bc6b94875
aUsernameThatWasntTaken <maxymilianbyzdra@gmail.com>
2025-01-11 11:44:59 +0000

    More work on chatbot, restructured lexicon

--- chatbot.py
+++ chatbot.py
@@ -6,15 +6,20 @@ class LanguageSyntax:
         for structure, structureDefinition in jsonDict.items():
             self.dict[structure] = structureDefinition
 
+class LanguageLexicon:
+    def __init__(self, jsonDict:dict[str,list[dict[str,str]]]):
+        self.dict = jsonDict
+
 def wierdFunctionINeed(inputList: list[str], n: int):
     """
     function that returns a list of all the ways to split a list into n lists.\n
     I already hate this project.\n
-    edit: I had to rewrite this godforsaken garbage for n != 3\n
+    I had to rewrite this godforsaken garbage for n != 3\n
     I hate my life.\n
     DONOTEDIT
+    TODO: Rename this and the other function to something better.
     """
-    returnList: list[list] = []
+    returnList: list[list[list[str]]] = []
     for numbersPossibility in functionThatHelpsTheAbove(n,len(inputList)):
         iterator = iter(inputList)
         possibility: list[list[str]] = []
@@ -49,28 +54,36 @@ def functionThatHelpsTheAbove(n:int, i:int):
 
 def checkStructure(text: list[str], type: str):
     if type not in languageSyntax.dict.keys():
-        raise NotImplementedError("""This isn't a language structure type. I will soon 
-                                  implement this to look for word classes, but not today.""")
+        if type not in languageLexicon.dict.keys():
+            raise ValueError(f"languageSyntax file contains unsupported Structure or word class: {type}")
+        textStr = " ".join(text)
+        if textStr in [word["root"] for word in languageLexicon.dict[type]]:
+            return (type, textStr)
+        else:
+            return None
     for structure in languageSyntax.dict[type]:
         textVariations = wierdFunctionINeed(text,len(structure))
-        print(textVariations,structure)
-        for variation,element in zip(textVariations, structure):
-                #YAY! Recursion. :ε
-                if element[0] == "?":
-                    element = element.removeprefix("?")
-                    #Make this optional
-                checkStructure(variation,element)
-        #use wierdFunctionINeed to split text into various combinations, and check them against the structure.
-        #TODO: Add an object constructor for the syntax to make this more predictable.
+        for variation in textVariations:
+            for itemToBeChecked,element in zip(variation, structure):
+                    #YAY! Recursion. :ε
+                    if element[0] == "?":
+                        element = element.removeprefix("?")
+                        #Make this optional
+                    if element[0] == "*":
+                        element = element.removeprefix("*")
+                    checkStructure(itemToBeChecked,element)
+            #use wierdFunctionINeed to split text into various combinations, and check them against the structure.
+        #TODO: Use the object constructor for the syntax to make this more predictable. (mostly done)
         #Make this return something like a tuple of something to identify the structure found, or raise an exception if none is found.
 
 #define a custom exception (structureNotFoundError)
 with open("languageSyntax.json") as f:
     languageSyntax = LanguageSyntax(json.load(f))
 with open("languageLexicon.json") as f:
-    languageLexicon = json.load(f)
+    languageLexicon = LanguageLexicon(json.load(f))
 
-prompt = input("Input: ")
+# prompt = input("Input: ")
+prompt = "The cat ate the dog" # for testing the code semi-automatically TODO: Add unit tests or something when this is more or less finished
 wordsList = prompt.split()
 #catch exceptions here
 checkStructure(wordsList, "sentence")

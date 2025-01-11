b418011a20e407e1bc86820f67139742546bc239
aUsernameThatWasntTaken <maxymilianbyzdra@gmail.com>
2025-01-11 14:44:33 +0000

    Added exception handling to chatbot_test

--- chatbot_test.py
+++ chatbot_test.py
@@ -1,10 +1,18 @@
 import unittest
-from chatbot import Bot
-
+try:
+    from chatbot import Bot
+except Exception as e:
+    print("Encountered exception: "+str(e))
+    exit(1)
 
 
 class testBot(unittest.TestCase):
     def test_doesntCrash(self):
-        fakeInput = iter(["The dog ate the cat","STOP"])
-        bot = Bot(fakeInput.__next__,print)
-        bot.run()
+        try:
+            fakeInput = iter(["The dog ate the cat","STOP"])
+            bot = Bot(fakeInput.__next__,print)
+            testSucceeded = True
+            bot.run()
+        except Exception:
+            testSucceeded = False
+        self.assertTrue(testSucceeded)
